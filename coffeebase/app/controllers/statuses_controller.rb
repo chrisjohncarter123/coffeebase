050e62c27711948ed5b8d20696d3b1e77c144886
chrisjohncarter123 <chrisjohncarter123@gmail.com>
2020-04-12 17:21:11 -0500

    updated status_controller

--- coffeebase/app/controllers/statuses_controller.rb
+++ coffeebase/app/controllers/statuses_controller.rb
@@ -23,6 +23,13 @@ class StatusesController < ApplicationController
     erb :'statuses/edit'
   end
 
+  delete "/statuses/:id/delete" do
+    redirect_if_not_logged_in
+    @status = Status.find(params[:id])
+    @status.destroy
+    redirect "/statuses"
+  end
+
   post "/statuses/:id" do
     redirect_if_not_logged_in
     @status = Status.find(params[:id])
